cefefc3cdcdf7aa0170e5c25312b11b66fea9d5a
sudolivictoria <ovquintanilla@istu.gob.sv>
2026-03-24 16:12:00 -0600

    Fix: System name

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Help Desk ISTU - Iniciar sesión</title>
+    <title>Help Desk ISTU</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
