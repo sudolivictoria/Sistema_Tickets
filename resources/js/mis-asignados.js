5c76aa43e424a1c6e7f04f5a80876d21e90717bf
sudolivictoria <ovquintanilla@istu.gob.sv>
2026-06-02 09:44:15 -0600

    Fix: bug mis asignados

--- resources/js/mis-asignados.js
+++ resources/js/mis-asignados.js
@@ -13,8 +13,7 @@ window.inicializarTablaTickets = function (
 
     $.fn.dataTable.ext.pager.numbers_length = 5;
     table = tableElement.DataTable({
-        stateSave: true,     
-        stateDuration: -1,
+        stateSave: false,     
         language: {
             processing: "Procesando...",
             lengthMenu: "Mostrar _MENU_ registros",
