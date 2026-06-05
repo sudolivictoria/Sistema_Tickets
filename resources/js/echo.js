b2409f6c0920e292cccf82dd2f1b651c45e32967
sudolivictoria <ovquintanilla@istu.gob.sv>
2026-06-05 09:48:38 -0600

    Echo development & production

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -3,17 +3,19 @@ import Pusher from "pusher-js";
 
 window.Pusher = Pusher;
 
+const esProduccion = window.location.pathname.startsWith('/sistema-tickets') || !['localhost', '127.0.0.1'].includes(window.location.hostname);
+
 window.Echo = new Echo({
     broadcaster: "reverb",
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
     
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? (esProduccion ? 80 : 8080),
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? (esProduccion ? 80 : 8080),
     
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "http") === "https",
     enabledTransports: ["ws", "wss"],
 
-    wsPath: "/sistema-tickets/app",
-    authEndpoint: "/sistema-tickets/api/broadcasting/auth",
+    wsPath: esProduccion ? "/sistema-tickets/app" : undefined,
+    authEndpoint: esProduccion ? "/sistema-tickets/api/broadcasting/auth" : "/broadcasting/auth",
 });
